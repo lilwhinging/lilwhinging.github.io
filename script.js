d9c35b44ff80f8db67185c715c3dd1fa23ca5536
littlewhinging <83141135+lilwhinging@users.noreply.github.com>
2023-02-16 00:11:23 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var pages = document.getElementsByClassName("page");
-var changePage = function(index){
-    for (let i = 0;i < pages.length;i++){
-        pages[i].style.display = "none";
-    }
-    pages[index].style.display = "block";
-};
-changePage(0);
-console.log("Hello there :)\n\n\n▒▒▒▒▒▒▒█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█\n▒▒▒▒▒▒▒█░▒▒▒▒▒▒▒▓▒▒▓▒▒▒▒▒▒▒░█\n▒▒▒▒▒▒▒█░▒▒▓▒▒▒▒▒▒▒▒▒▄▄▒▓▒▒░█░▄▄\n▒▒▄▀▀▄▄█░▒▒▒▒▒▒▓▒▒▒▒█░░▀▄▄▄▄▄▀░░█\n▒▒█░░░░█░▒▒▒▒▒▒▒▒▒▒▒█░░░░░░░░░░░█\n▒▒▒▀▀▄▄█░▒▒▒▒▓▒▒▒▓▒█░░░█▒░░░░█▒░░█\n▒▒▒▒▒▒▒█░▒▓▒▒▒▒▓▒▒▒█░░░░░░░▀░░░░░█\n▒▒▒▒▒▄▄█░▒▒▒▓▒▒▒▒▒▒▒█░░█▄▄█▄▄█░░█\n▒▒▒▒█░░░█▄▄▄▄▄▄▄▄▄▄█░█▄▄▄▄▄▄▄▄▄█\n▒▒▒▒█▄▄█░░█▄▄█░░░░░░█▄▄█░░█▄▄█");
