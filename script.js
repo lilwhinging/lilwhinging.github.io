30bbf29ccce83af59a3ef2a3ec2f7399d715e32d
littlewhinging <83141135+lilwhinging@users.noreply.github.com>
2023-02-24 09:14:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function reveal() {
+    var reveals = document.querySelectorAll(".main");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 20;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
+function reveal2() {
+    var reveal2s = document.querySelectorAll(".main2");
+    for (var i = 0; i < reveal2s.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveal2s[i].getBoundingClientRect().top;
+        var elementVisible = 20;
+        if (elementTop < windowHeight - elementVisible) {
+            reveal2s[i].classList.add("active");
+        }
+    }
+}
+window.addEventListener("scroll", reveal);
+reveal();
+window.addEventListener("scroll", reveal2);
+reveal2();
