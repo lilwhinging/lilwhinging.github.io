2db88706b3fc414b1b215a65f039c6542a05b16c
littlewhinging <83141135+lilwhinging@users.noreply.github.com>
2023-03-10 21:52:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-
-var menu = 0;
 function reveal() {
-        var reveals = document.querySelectorAll(".main");
-        for (var i = 0; i < reveals.length; i++) {
-            var windowHeight = window.innerHeight;
-            var elementTop = reveals[i].getBoundingClientRect().top;
-            var elementVisible = 20;
-            if (elementTop < windowHeight - elementVisible) {
-                reveals[i].classList.add("active");
-            }
+    var reveals = document.querySelectorAll(".main");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 20;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
         }
     }
+}
 function reveal2() {
     var reveal2s = document.querySelectorAll(".main2");
     for (var i = 0; i < reveal2s.length; i++) {
@@ -22,25 +20,16 @@ function reveal2() {
         }
     }
 }
-function doMenu(){
-    menu = 1-menu;
+function showMenu(){
     var showMenu = document.querySelectorAll(".menu-item");
     for (var i =0; i < showMenu.length; i++) {
-        if (menu == 1) {
-            showMenu[i].classList.add("active");
-        } 
-        else {
-            showMenu[i].classList.remove("active");
-        }
+        showMenu[i].classList.add("active");
     }
-    showMenu = document.getElementById("menu");
-    if (menu == 1) {
-        showMenu.classList.add("active");
-        showMenu.innerHTML = "close";
-    }
-    else {
-        showMenu.classList.remove("active");
-        showMenu.innerHTML = "menu";
+}
+function hideMenu(){
+    var showMenu = document.querySelectorAll(".menu-item");
+    for (var i =0; i < showMenu.length; i++) {
+        showMenu[i].classList.remove("active");
     }
 }
 window.addEventListener("scroll", reveal);
