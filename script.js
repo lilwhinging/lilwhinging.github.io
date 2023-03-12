8273376c95e37b81384ff1b43b0416c6f9522274
littlewhinging <83141135+lilwhinging@users.noreply.github.com>
2023-03-12 10:56:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -21,16 +21,8 @@ function reveal2() {
     }
 }
 function showMenu(){
-    var showMenu = document.querySelectorAll(".menu-item");
-    for (var i =0; i < showMenu.length; i++) {
-        showMenu[i].classList.add("active");
-    }
-}
-function hideMenu(){
-    var showMenu = document.querySelectorAll(".menu-item");
-    for (var i =0; i < showMenu.length; i++) {
-        showMenu[i].classList.remove("active");
-    }
+    var menuI = document.getElementById("menu-bar");
+    menuI.classList.toggle("active");
 }
 window.addEventListener("scroll", reveal);
 reveal();
